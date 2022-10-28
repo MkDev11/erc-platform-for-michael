8cbe93460c8fee84be06ec790b28537bf8efe14f
mkdev11 <jaysmth689@gmail.com>
2022-10-28 17:03:40 -0400

    remove helmet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import { BrowserRouter } from "react-router-dom";
-import { Helmet } from "react-helmet";
 import ErrorPage from "pages/error";
 import SuspenseFallback from "components/suspense";
 import AppRoutes from "routes";
@@ -11,7 +10,6 @@ const App = () => {
     <ErrorBoundary FallbackComponent={ErrorPage}>
       <BrowserRouter>
         <Suspense fallback={<SuspenseFallback />}>
-         
           <AppRoutes />
         </Suspense>
       </BrowserRouter>
